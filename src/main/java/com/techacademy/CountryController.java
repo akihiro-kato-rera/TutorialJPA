2598a26a1f4774bd52568cedda5133a5f204aaf7
akihiro-kato-rera <tainakaritu19910821@yahoo.co.jp>
2025-05-29 00:02:53 +0900

    レッスン１６の課題

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -28,7 +28,7 @@ public class CountryController {
 
     //-----追加：ここから-----
     //-----詳細画面------
-    @GetMapping(value = {"/detail","/detail/{code}/"})
+    @GetMapping(value = {"/detail/{code}","/detail/{code}/"})
     public String getCountry(@PathVariable(name="code",required=false)String code,Model model) {
        //codeが指定されていたら検索結果、無ければ空のクラスの設定
         Country country = code != null ? service.getCountry(code) : new Country();
@@ -47,8 +47,10 @@ public class CountryController {
     }
 
     //-------削除画面-----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = "/delete/{code}")
+    public String deleteCountryForm(@PathVariable(name="code",required=false)String code, Model model) {
+        Country country = code != null ? service.getCountry(code) : new Country();
+        model.addAttribute("country",country);
         return "country/delete";
 
     }
